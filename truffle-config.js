f3c8d30e04ebab17f2a169080cbc2b0626d5e59f
ujjwalnp <dhakalujjwal99@gmail.com>
2024-01-05 14:08:55 +0545

    fix(dev-env): solc version

--- truffle-config.js
+++ truffle-config.js
@@ -2,16 +2,17 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1", 
+      host: '127.0.0.1', 
       port: 7545, 
-      network_id: "*",
+      network_id: '*',
+      gas: 6721975,
     },
     
   },
 
   compilers: {
     solc: {
-      version: "^0.8.21",
+      version: '0.5.1',
        optimizer: {
          enabled: true,
          runs: 200
@@ -19,7 +20,7 @@ module.exports = {
     },
   },
 
-  contracts_directory: "./contracts/",  
-  contracts_build_directory: "./src/abis/",
+  contracts_directory: './src/contracts/',  
+  contracts_build_directory: './src/abis/',
 
 };
