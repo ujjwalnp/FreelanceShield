708c072f073ac12a3404b850338cc9bf1372f122
Ankit Rayamajhi <ankitrayamajhi518@gmail.com>
2024-01-22 19:52:10 +0545

    feat(deploy): deploy smart contract to Ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,26 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const mnemonic = process.env.MNEMONIC;
+const infuraApiKey = process.env.INFURA_API_KEY;
 
 module.exports = {
   networks: {
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraApiKey}`),
+      network_id: 3,
+    },
     development: {
-      host: '127.0.0.1', 
-      port: 7545, 
+      host: '127.0.0.1',
+      port: 7545,
       network_id: '*',
       gas: 6721975,
     },
-    
+    sepolia: {
+      provider: () => new HDWalletProvider(mnemonic, `https://sepolia.infura.io/v3/${infuraApiKey}`),
+      networkCheckTimeout: 100000 ,
+      network_id: 11155111,
+    },
   },
 
   compilers: {
