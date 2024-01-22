2d02eb935f422b29c28c9987cdb880e131694b4b
ujjwalnp <dhakalujjwal99@gmail.com>
2024-01-22 19:50:49 +0545

    fix(frontend): contract page styling

--- src/Components/Contract.js
+++ src/Components/Contract.js
@@ -69,6 +69,8 @@ export function Contract({
     } else if (role == "client" && !contract.clientSigned) {
       return (
         <button
+          type="button"
+          className="bg-green-100 text-green-800 text-sm font-semibold me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300"
           onClick={() =>
             depositSecurityDeposit(
               role,
